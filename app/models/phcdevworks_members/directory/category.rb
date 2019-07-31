86e9f5847f234d290bddff12bde7d32499838fa0
BradPotts <brad.potts@phcnet.co>
2019-07-31 23:39:11 +0000

    fixes to category.

--- app/models/phcdevworks_members/directory/category.rb
+++ app/models/phcdevworks_members/directory/category.rb
@@ -1,25 +1,25 @@
 module PhcdevworksMembers
-	class Directory::Category < ApplicationRecord
+  class Directory::Category < ApplicationRecord
 
-		# Clean URL Initialize
-		extend FriendlyId
+    # Clean URL Initialize
+    extend FriendlyId
 
-		# Paper Trail Initialize
-		has_paper_trail :class_name => 'PhcdevworksMembers::CategoryVersions'
+    # Paper Trail Initialize
+    has_paper_trail :class_name => 'PhcdevworksMembers::CategoryVersions'
 
-		# Relationships
-		has_and_belongs_to_many :listings, class_name: 'PhcdevworksMembers::Member::Listing', :join_table => 'categories_listings', :dependent => :destroy
+    # Relationships
+    has_and_belongs_to_many :listings, class_name: 'PhcdevworksMembers::Member::Listing', :join_table => 'categories_listings', :dependent => :destroy
 
-		# Form Fields Validation
-		validates :category_name,
-			presence: true
+    # Form Fields Validation
+    validates :category_name,
+      presence: true
 
-		# Clean URL Define
-		friendly_id :phc_nice_url_slug, use: [:slugged, :finders]
+    # Clean URL Define
+    friendly_id :phc_nice_url_slug, use: [:slugged, :finders]
 
-		def phc_nice_url_slug
-			[:category_name]
-		end
+    def phc_nice_url_slug
+      [:category_name]
+    end
 
-	end
+  end
 end
