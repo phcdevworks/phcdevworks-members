0db2f4d3fade641eba0ab3c3e2a7d4135651be3f
BradPotts <brad.potts@phcnet.co>
2019-10-14 12:17:58 +0000

    adjusted menu and minor test_app adjustment.

--- test/test_app/app/assets/config/manifest.js
+++ test/test_app/app/assets/config/manifest.js
@@ -1,8 +1,7 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link phcdevworks_members_manifest.js
-//= link phcdevworks_members/application.css
-//= link phcdevworks_members/application.js
-//= link phcdevworks_accounts_manifest.js
 //= link phcdevworks_accounts/application.css
 //= link phcdevworks_accounts/application.js
+//= link phcdevworks_members/application.css
+//= link phcdevworks_members/application.js
