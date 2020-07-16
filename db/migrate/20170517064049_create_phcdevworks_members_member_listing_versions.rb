ad3b0536d1858cbd7643cf2543a71a69fc6aa49f
BradPotts <brad.potts@phcnet.co>
2020-07-16 23:33:54 +0000

    updated sidebar.

--- db/migrate/20170517064049_create_phcdevworks_members_member_listing_versions.rb
+++ db/migrate/20170517064049_create_phcdevworks_members_member_listing_versions.rb
@@ -3,14 +3,12 @@ class CreatePhcdevworksMembersMemberListingVersions < ActiveRecord::Migration[6.
   def change
 
     create_table :phcdevworks_members_member_listing_versions do |t|
-
       t.string   :item_type, {:null=>false}
       t.integer  :item_id,   null: false
       t.string   :event,     null: false
       t.string   :whodunnit
       t.text     :object, limit: TEXT_BYTES
       t.datetime :created_at
-
     end
 
     add_index :phcdevworks_members_member_listing_versions, %i(item_type item_id), :name => 'member_listing_versions'
